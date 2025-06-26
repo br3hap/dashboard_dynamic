1aa5d1b68131868575c16b32ec7c370bd88a7c32
Breithner <Breithnerap@gmail.com>
2025-06-26 16:08:08 -0500

    upload change

--- models/dashboard_block.py
+++ models/dashboard_block.py
@@ -43,4 +43,30 @@ class DashboardBlock(models.Model):
     )
     measured_field_id = fields.Many2one('ir.model.fields', string='Campo medido')
     client_action_id = fields.Many2one('ir.actions.client', string='Acción de cliente', default=get_default_action)
+    type = fields.Selection(
+        string=_('Tipo'),
+        selection=[
+            ('graph', 'Chart'),
+            ('tile', 'Tile'),
+        ],
+    )
+    x_axis = fields.Char(string=_('X-Axis'))
+    y_axis = fields.Char(string=_('Y-Axis'))
+    height = fields.Char(string=_('Height'))
+    width = fields.Char(string=_('Width'))
+    translate_x = fields.Char(string=_('Translate X'))
+    translate_y = fields.Char(string=_('Translate Y'))
+    data_x = fields.Char(string=_('Data X'))
+    data_y = fields.Char(string=_('Data Y'))
+    data_y = fields.Char(string=_('Data Y'))
+    group_by_id = fields.Many2one('ir.model.fields', string='Group by (Y- Axis)')
+    tile_color = fields.Char(string=_('Tile Color'))
+    text_color = fields.Char(string=_('Text Color'))
+    val_color = fields.Char(string=_('Valor del Color'))
+    fa_color = fields.Char(string=_('Icono del Color'))
+    filter = fields.Char(string=_('Filter'))
+    model_id = fields.Many2one('ir.model', string='Model')
+    model_name = fields.Char(related='model_id.model' ,string=_('Nombre del modelo'))
+    edit_mode = fields.Boolean(string=_('Modo editar'))
+
 
