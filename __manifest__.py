5941c6c4b0502620d0e86b380815139fd917c63d
Breithner <Breithnerap@gmail.com>
2025-06-26 15:36:25 -0500

    upload change

---
+++ __manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Dashboard_dynamic',
+    'version': '',
+    'summary': """ Dashboard Dinámico - odoo 18 """,
+    'author': '',
+    'website': '',
+    'category': '',
+    'depends': ['base', ],
+    'data': [
+        
+    ],
+    
+    'application': True,
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
